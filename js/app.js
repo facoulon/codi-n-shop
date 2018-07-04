d23f218a1f747dac244a1a92413bd5bfdb1887f4
facoulon <f.coulon48@laposte.net>
2018-07-04 10:04:00 +0200

    slider head done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
+
+  var sliderWidth=$(".sliderHead div").width()
+  $(".sliderHead div").height(sliderWidth/4);
   $(".slider").bxSlider({
-    auto: true,
+    auto:true,
     tickerHover: true,
-    
+    adaptiveHeight:true,
+    pager:false,
+    controls:false,
   });
+
+setInterval(function() {
+var sliderWidth=$(".sliderHead div").width()
+var sliderHeight=$(".sliderHead div").height(sliderWidth/4);
+var imgHeight=$(".sliderHead img").height()
+var marginTop = (imgHeight-sliderWidth/4)/-2;
+marginTop +='px';
+$(".sliderHead img").css('margin-top', marginTop);
+},300);
+
+
+
+
 
 
 
