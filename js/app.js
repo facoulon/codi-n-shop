e3180ec00a5bb64b0da45f837d8c1bf61364cb44
hermelen <Hermelen>
2018-07-05 14:30:07 +0200

    generation article static jquery

--- js/app.js
+++ js/app.js
@@ -20,10 +20,10 @@ $(document).ready(function() {
   }, 300);
 
   var filterWidth = $("#filterMenu").width()
-  $("#filterMenu").css('left', -filterWidth-32);
+  $("#filterMenu").css('left', -filterWidth - 32);
 
   $(".filter").click(function(event) {
-      if ($("#filterMenu").css('position') == 'absolute') {
+    if ($("#filterMenu").css('position') == 'absolute') {
       $("#filterMenu").css('left', '0');
       $("#filterMenu").css('position', 'relative');
       $("#menuArticle").attr('class', 'column col-12 col-sm-7 col-md-9');
@@ -32,19 +32,39 @@ $(document).ready(function() {
         'margin-left': '0.2em',
         'transform': 'rotate(180deg)'
       });
-      } else {
-        var filterWidth = $("#filterMenu").width()
-        $("#filterMenu").css('left', -filterWidth-32);
-        var filterHeight = $("#menuArticle").height();
-        console.log(filterHeight);
-        $("#filterMenu").css('height', filterHeight+'px');
-        $("#filterMenu").css('position', 'absolute');
-        $("#menuArticle").attr('class','column col-12');
-        $(".filter").attr('class', 'col filter');
-        $(".filter i").css({'margin-left':'0',
-          'transform':'initial'});
+    } else {
+      var filterWidth = $("#filterMenu").width()
+      $("#filterMenu").css('left', -filterWidth - 32);
+      var filterHeight = $("#menuArticle").height();
+      console.log(filterHeight);
+      $("#filterMenu").css('height', filterHeight + 'px');
+      $("#filterMenu").css('position', 'absolute');
+      $("#menuArticle").attr('class', 'column col-12');
+      $(".filter").attr('class', 'col filter');
+      $(".filter i").css({
+        'margin-left': '0',
+        'transform': 'initial'
+      });
     }
   });
+
+  // ========================== MANAGE DATABASE ==================================
+  for (var i = 0; i < 100; i++) {
+    $("div#menuArticle div.row div.column.col-12#card-art div.row").append('<div class="column col-12 col-sm-6 col-lg-3 articleVente" id="produit-' + i + '">')
+    var catalogProd = $("div.column.articleVente#produit-"+ i);
+    catalogProd.append("<h2>Trust Gaming GXT 105 Izza</h2>");
+    catalogProd.append('<img class="img-fluid" src="img/souristest.jpg" alt="">')
+    catalogProd.append('<span class="prix">90$</span>')
+    catalogProd.append('<span class="description">Taillée pour le jeu, et avec un dessus entièrement éclairé</span>')
+    catalogProd.append('<a href = "produit-' + i + '"></a>')
+    catalogProd.append('<section id="section-' + i + '">')
+    var catalogSection = $("#section-" + i)
+    catalogSection.append('<button type="button" name="button">+</button>')
+    catalogSection.append('<input class="cpt" type="text" name="" value="">')
+    catalogSection.append('<button type="button" name="button">-</button>')
+    catalogProd.append('<button class="ajoutPanier" type="button" name="button">Ajout au panier</button>')
+  }
+
 
 
 
